e0aa643e5a53e5501dc7009533482e7b51ec911b
Miguel Angel Rivera <angel1301999@gmail.com>
2020-10-21 01:16:17 -0500

    divicion de tcp en sesiones

---
+++ src/main/Session.java
@@ -0,0 +1,132 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.UUID;
+
+import com.google.gson.Gson;
+
+import model.Disparo;
+import model.Jugador;
+import model.Posicion;
+import model.Vida;
+
+public class Session extends Thread {
+
+	private String id;
+	private BufferedReader reader;
+	private Socket socket;
+	private OnMessageListener observer;
+	
+	//jugador
+		Jugador jugador;
+		
+		//posicion
+		Posicion posicion;
+		
+		//disparo
+		Disparo disparo;
+		
+		//vida
+		Vida vida;
+		
+		
+		Gson gson;
+
+	public Session(Socket socket) {
+		super();
+		this.socket = socket;
+		this.id = UUID.randomUUID().toString();
+		
+	}
+
+	@Override
+	public void run() {
+		
+		try {
+			
+			//Input Output
+			OutputStream os= socket.getOutputStream();
+			InputStream is=socket.getInputStream();
+			
+			
+			//Writer reader
+			reader= new BufferedReader(new InputStreamReader(is));
+			
+			
+			//Recepcion de datos
+			while(true) {
+		    	
+		    	System.out.println("Esperando mensaje");
+		
+		    	//Recibimos el mensaje
+		    	String mensaje;
+				mensaje = reader.readLine();
+				
+				//gson para deserializar el mensaje
+				Gson gson=new Gson();
+				Generic generic= gson.fromJson(mensaje, Generic.class);
+				
+				
+				//Se deserializa segun el tipo de dato que se envio
+				if(mensaje!=null) {
+				switch(generic.type) {
+				
+				case "Jugador": 
+					jugador=gson.fromJson(mensaje, Jugador.class);
+					
+					observer.messageJugador(jugador);
+					
+					System.out.println(jugador.personaje);
+					break;
+					
+				case "Posicion": 
+					posicion=gson.fromJson(mensaje, Posicion.class);
+					
+					observer.messagePosicion(posicion);
+					
+					System.out.println(posicion.getX());
+					break;
+					
+				case "Disparo": 
+					disparo=gson.fromJson(mensaje, Disparo.class);
+					
+					observer.messageDisparo(disparo);
+					
+					break;
+			
+				case "Vida":
+					vida=gson.fromJson(mensaje, Vida.class);
+					observer.messageVida(vida);
+					break;
+					
+				}
+		       
+		    }
+				//System.out.println(mensaje);
+		    
+		    }
+			
+			
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void setObserver(OnMessageListener observer2) {
+		// TODO Auto-generated method stub
+		
+		this.observer = observer;
+		
+	}
+	
+	public String getID() {
+		return this.id;
+	}
+
+}
--- src/main/TCPSingleton.java
+++ src/main/TCPSingleton.java
@@ -18,169 +18,55 @@ import model.Vida;
 import com.google.gson.Gson;
 
 public class TCPSingleton extends Thread {
-	
+
 	private static TCPSingleton unicaInstancia;
-	
-	//Socket
+
+	// Socket
 	private ServerSocket server;
-	
-	//jugador
-	Jugador jugador;
-	
-	//posicion
-	Posicion posicion;
-	
-	//disparo
-	Disparo disparo;
-	
-	//vida
-	Vida vida;
-	
 
-	
-	private TCPSingleton () {
-		
+	private TCPSingleton() {
+
 	}
-	
-	
-	//Instance
-		public static TCPSingleton getInstance() {
-			if (unicaInstancia==null) {
-				unicaInstancia= new TCPSingleton();
-				unicaInstancia.start();
-			}
-			return unicaInstancia;
+
+	// Instance
+	public static TCPSingleton getInstance() {
+		if (unicaInstancia == null) {
+			unicaInstancia = new TCPSingleton();
+			unicaInstancia.start();
 		}
-		
-	
-	
-	//writer and reader
-	BufferedWriter writer;
-	BufferedReader reader;
-	
-	
-	Gson gson;
-	
-	
-	
-	//Observer
-	private OnMessageListener observer;
-	
-	public void setObserver(OnMessageListener observer) {
-		this.observer=observer;
+		return unicaInstancia;
 	}
-	
-	
 
-	
-	//Hilo conexion
-	public void run () {
-		
+	// Observer
+	private OnMessageListener observer;
+
+	public void setObserver(OnMessageListener observer) {
+		this.observer = observer;
+	}
+
+	// Hilo conexion
+	public void run() {
+
 		try {
-			
-			//Conexión
+
+			// Conexión
 			server = new ServerSocket(5000);
-			System.out.println("Esperando conexión");
-			Socket socket= server.accept();
-			System.out.println("Conectado con el usuario");
-			
-			
-			//Input Output
-			OutputStream os= socket.getOutputStream();
-			InputStream is=socket.getInputStream();
-			
-			
-			//Writer reader
-			writer=new BufferedWriter(new OutputStreamWriter(os));
-			reader= new BufferedReader(new InputStreamReader(is));
-			
-			
-			//Recibo el mensaje 
-		    recibir();
-		   		
+
+			while (true) {
+
+				System.out.println("Esperando conexión");
+				Socket socket = server.accept();
+				Session session = new Session(socket);
+				session.setObserver(observer);
+				session.start();
+				System.out.println("Conectado a el usuario dentro de sesion");
+
+			}
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
-		
-	}
-	
-	
-	
-	
-	//Metodo recepcion mensajes
-	public void recibir() {
-		
-		
-		new Thread (
-				()->{
-					
-					try {
-				    while(true) {
-				    	
-				    	System.out.println("Esperando mensaje");
-				
-				    	//Recibimos el mensaje
-				    	String mensaje;
-						mensaje = reader.readLine();
-						
-						//gson para deserializar el mensaje
-						Gson gson=new Gson();
-						Generic generic= gson.fromJson(mensaje, Generic.class);
-						
-						
-						//Se deserializa segun el tipo de dato que se envio
-						if(mensaje!=null) {
-						switch(generic.type) {
-						
-						case "Jugador": 
-							jugador=gson.fromJson(mensaje, Jugador.class);
-							
-							observer.messageJugador(jugador);
-							
-							System.out.println(jugador.personaje);
-							break;
-							
-						case "Posicion": 
-							posicion=gson.fromJson(mensaje, Posicion.class);
-							
-							observer.messagePosicion(posicion);
-							
-							System.out.println(posicion.getX());
-							break;
-							
-						case "Disparo": 
-							disparo=gson.fromJson(mensaje, Disparo.class);
-							
-							observer.messageDisparo(disparo);
-							
-							break;
-					
-						case "Vida":
-							vida=gson.fromJson(mensaje, Vida.class);
-							observer.messageVida(vida);
-							break;
-							
-						}
-				       
-				    }
-						//System.out.println(mensaje);
-				    
-				    }
-				    
-				    } catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				
-				}
-				
-				).start();
-	
-		
-	}
+		}
 
-	
-
-	
+	}
 }
